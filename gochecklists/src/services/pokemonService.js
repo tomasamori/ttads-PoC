607839fda4ff186c1c6d26c7187aa96f6b1d2c81
tomasamori <tomasamori15@gmail.com>
2022-11-01 17:56:18 -0300

    update pokemon limit to 150

--- gochecklists/src/services/pokemonService.js
+++ gochecklists/src/services/pokemonService.js
@@ -1,6 +1,6 @@
 export const getPokemons = async () => {
     try {
-        const url = 'https://pokeapi.co/api/v2/pokemon?limit=500&offset=0';
+        const url = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
         const response = await fetch(url);
         const data = await response.json();
         return data
